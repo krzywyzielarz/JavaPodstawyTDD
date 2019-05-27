70c7bd6788e2bb79b63dc7c5b1f217d049ecc0b7
Grzegorz Rak <krzwyzielarz@gmail.com>
2019-05-27 22:26:33 +0200

    test git commit

--- src/main/java/pl/grak/java/podsawy/j001/Main.java
+++ src/main/java/pl/grak/java/podsawy/j001/Main.java
@@ -2,6 +2,7 @@ package pl.grak.java.podsawy.j001;
 
 public class Main {
     public static void main(String[] ags){
+        System.out.println("test");
 
     }
 
---
+++ src/main/java/pl/grak/java/podsawy/w3resource/BasicPart1/Exercises001.java
@@ -0,0 +1,4 @@
+package pl.grak.java.podsawy.w3resource.BasicPart1;
+
+public class Exercises001 {
+}
